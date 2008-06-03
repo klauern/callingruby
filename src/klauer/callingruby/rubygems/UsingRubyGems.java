eccdf5dfc864f624040e27f4d47b65c732ea24e4
A03182 - Nick Klauer <nicholasklauer@alliantenergy.com>
2008-06-03 11:37:21 -0500

    Small changes to clean up and simplify the codebase.

--- src/klauer/callingruby/rubygems/UsingRubyGems.java
+++ src/klauer/callingruby/rubygems/UsingRubyGems.java
@@ -19,32 +19,30 @@ import org.jruby.javasupport.JavaEmbedUtils;
 public class UsingRubyGems {
 
    public List<String> jruby_properties;
-
    public static Ruby runtime;
    public static RubyRuntimeAdapter evaler;
 
    /* I guess I don't need this String at all...
    private static String ruby_classpath = ";C:\\Development\\Ruby\\jruby-1.1.1\\lib;" +
-           "C:\\Development\\Ruby\\jruby-1.1.1\\lib\\ruby\\site_ruby\\1.8; " +
-           "C:\\Development\\Ruby\\jruby-1.1.1\\lib\\ruby\\site_ruby\\1.8\\java" +
-           "C:\\Development\\Ruby\\jruby-1.1.1\\lib\\ruby\\site_ruby;" +
-           "C:\\Development\\Ruby\\jruby-1.1.1\\lib\\ruby\\1.8;" +
-           //"C:\\Development\\Ruby\\jruby-1.1.1\\lib\\ruby\\1.8\\java;" +
-           //"lib\\ruby\\1.8;" +
-           //"C:\\Development\\Ruby\\jruby-1.1.1\\bin\\..\\lib\\bsf.jar" +
-           "C:\\Development\\Ruby\\jruby-1.1.1\\bin\\..\\lib\\jruby.jar";
+   "C:\\Development\\Ruby\\jruby-1.1.1\\lib\\ruby\\site_ruby\\1.8; " +
+   "C:\\Development\\Ruby\\jruby-1.1.1\\lib\\ruby\\site_ruby\\1.8\\java" +
+   "C:\\Development\\Ruby\\jruby-1.1.1\\lib\\ruby\\site_ruby;" +
+   "C:\\Development\\Ruby\\jruby-1.1.1\\lib\\ruby\\1.8;" +
+   //"C:\\Development\\Ruby\\jruby-1.1.1\\lib\\ruby\\1.8\\java;" +
+   //"lib\\ruby\\1.8;" +
+   //"C:\\Development\\Ruby\\jruby-1.1.1\\bin\\..\\lib\\bsf.jar" +
+   "C:\\Development\\Ruby\\jruby-1.1.1\\bin\\..\\lib\\jruby.jar";
     */
-
    public static void main(String[] args) {
 
-      String classpath = System.getProperty("java.class.path");
+      //String classpath = System.getProperty("java.class.path");
       // See the Gotchas in CallSomeRuby.java
       //System.setProperty("jruby.base", "C:\\Development\\Ruby\\jruby-1.1.1"); // the jruby-1.1/ directory
       System.setProperty("jruby.home", "C:\\Development\\Ruby\\jruby-1.1.1\\bin\\..");   // wherever your /bin dir is for JRuby
       System.setProperty("jruby.shell", "cmd.exe");  // /bin/sh or cmd.exe depending
       System.setProperty("jruby.lib", "C:\\Development\\Ruby\\jruby-1.1.1\\bin\\..\\lib");  // jruby-1.1/lib (Must be relative, not explicit)
       System.setProperty("jruby.script", "jruby.bat");          //  org.jruby.Main %JRUBY_OPTS% %_RUBY_OPTS%"); // either jruby or jruby.bat org.jruby.Main %JRUBY_OPTS% %_RUBY_OPTS%
-      System.setProperty("java.class.path", classpath);
+      //System.setProperty("java.class.path", classpath);
       //System.getProperties("java.library.path");
       runtime = JavaEmbedUtils.initialize(new ArrayList());
       evaler = JavaEmbedUtils.newRuntimeAdapter();
